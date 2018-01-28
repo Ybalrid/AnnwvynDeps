d1c3b9153cce3f641c729167049cdfaf465ed02e
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2018-01-28 16:54:51 +0100

    Make the availability of a debug version of tinyxml2 optional
    Signed-off by: Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>

--- CMakeModules/FindTinyXML2.cmake
+++ CMakeModules/FindTinyXML2.cmake
@@ -6,15 +6,15 @@
 #This file expect you to set TinyXML2_ROOT to the output folder of the "INSTALL" target built with
 #CMake on the TinyXML2 project. And It expects you to use TinyXML2 as a *static* library
 
-find_path (TinyXML2_INCLUDE_DIR tinyxml2.h 
+find_path (TinyXML2_INCLUDE_DIR tinyxml2.h
     HINTS ${TinyXML2_ROOT}/include)
 
-find_library(TinyXML2_LIBRARY tinyxml2.lib libtinyxml2.a tinyxml2 libtinyxml2 
-    HINTS ${TinyXML2_ROOT}/lib 
+find_library(TinyXML2_LIBRARY tinyxml2.lib libtinyxml2.a tinyxml2 libtinyxml2
+    HINTS ${TinyXML2_ROOT}/lib
     PATH_SUFFIXES tinyxml2 tinyxml2/bin tinyxml2/bin/tinyxml2/ tinyxml2/bin/tinyxml2/x64-Release-Lib)
 
 find_library(TinyXML2_LIBRARY_DEB tinyxml2d.lib libtinyxml2d.a tinyxml2d libtinyxml2d
-    HINTS ${TinyXML2_ROOT}/lib 
+    HINTS ${TinyXML2_ROOT}/lib
     PATH_SUFFIXES tinyxml2 tinyxml2/bin tinyxml2/bin/tinyxml2/ tinyxml2/bin/tinyxml2/x64-Debug-Lib)
 
 include(FindPackageHandleStandardArgs)
@@ -23,5 +23,8 @@ find_package_handle_standard_args(TinyXML2 DEFAULT_MSG TinyXML2_LIBRARY TinyXML2
 mark_as_advanced(TinyXML2_INCLUDE_DIR TinyXML2_LIBRARY TinyXML2_LIBRARY_DEB)
 
 set (TinyXML2_INCLUDE_DIRS ${TinyXML2_INCLUDE_DIR})
-set (TinyXML2_LIBRARIES optimized ${TinyXML2_LIBRARY} debug ${TinyXML2_LIBRARY_DEB})
-    
+if(NOT TinyXML2_LIBRARY_DEB)
+    set (TinyXML2_LIBRARIES ${TinyXML2_LIBRARY})
+else()
+    set (TinyXML2_LIBRARIES optimized ${TinyXML2_LIBRARY} debug ${TinyXML2_LIBRARY_DEB})
+endif()
